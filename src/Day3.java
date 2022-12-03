b8603d2cc84570470c2435359dd80eba6315dcca
fboelens <fboelens@gmail.com>
2022-12-03 13:17:28 +0100

    day 3 added

--- src/Day3.java
+++ src/Day3.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,13 +11,13 @@ public class Day3 {
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            ArrayList arr = new ArrayList();
+            ArrayList<String> arr = new ArrayList<>();
             String st;
             int value = 0;
             int len;
             while ((st = br.readLine()) != null) {
                 len = st.length() / 2;
-                value += (int) getMatch(st.substring(0, len),st.substring(len)).get(0);
+                value += (int) getMatch(st.substring(0, len),st.substring(len)).get("value");
                 arr.add(st);
             }
 
@@ -26,11 +25,11 @@ public class Day3 {
 
             // question 2
             int chunk = 3;
-            String firstMatch = "";
+            String firstMatch;
             value = 0;
             for(int i=0;i<arr.size();i+=chunk){
-                firstMatch = (String) getMatch((String) arr.get(i),(String) arr.get(i+1)).get(1);
-                value += (int) getMatch (firstMatch,(String) arr.get(i+2)).get(0);
+                firstMatch = (String) getMatch(arr.get(i),arr.get(i+1)).get("string");
+                value += (int) getMatch (firstMatch,arr.get(i+2)).get("value");
             }
             System.out.println("Day 3 - Question 2: "+value);
         }
@@ -40,10 +39,10 @@ public class Day3 {
     }
 
     // Function to count the matching characters
-    static ArrayList getMatch(String str1, String str2)
+    static HashMap<String, Object> getMatch(String str1, String str2)
     {
-        ArrayList arr = new ArrayList();
-        int c = 0, j = 0;
+        HashMap<String, Object> result = new HashMap<>();
+        int c = 0;
         int value = 0;
         // Traverse the string 1 char by char
         String skip = "";
@@ -52,7 +51,7 @@ public class Day3 {
          if (!skip.contains(""+str1.charAt(i)) && str2.contains(""+str1.charAt(i)))
             {
                 c += 1;
-                int a = ((int) (str1.charAt(i)));
+                int a = (str1.charAt(i));
                 if (a<97) {
                     a=a-64+26;
                 }
@@ -63,9 +62,9 @@ public class Day3 {
                 value+=a;
             }
         }
-        arr.add(value);
-        arr.add(skip);
-        arr.add(c);
-        return arr;
+        result.put("value" ,value);
+        result.put("string", skip);
+        result.put("matches" , c);
+        return result;
     }
 }
