ae63bc2025e3da35c5c418b679394bc2492dee6c
fboelens <fboelens@gmail.com>
2022-12-04 11:25:01 +0100

    day 4 added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         //Day1.execute();
         //Day2.execute();
-        Day3.execute();
+        //Day3.execute();
+        Day4.execute();
     }
 }
