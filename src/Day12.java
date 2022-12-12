beb0fcd5fd8e9c5c3eb5bbdc5b082f29494f40e0
fboelens <boelens.f@tkppensioen.nl>
2022-12-12 14:46:28 +0100

    day 12 part two

--- src/Day12.java
+++ src/Day12.java
@@ -8,6 +8,8 @@ public class Day12 {
 
         File file = new File("resources/day12.txt");
         ArrayList<String> map = new ArrayList<>();
+        ArrayList<Node> allNodes = new ArrayList<>();
+        ArrayList<Node> startingNodes = new ArrayList<>();
         int rows = 0;
         Node newNode;
         Node source = new Node("");
@@ -16,7 +18,7 @@ public class Day12 {
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
-            ArrayList<Node> allNodes = new ArrayList<>();
+
             while ((st = br.readLine()) != null) {
 
 
@@ -30,80 +32,104 @@ public class Day12 {
                     if (st.charAt(cols)=='E') {
                         target = newNode;
                         st = st.replace('E','z');
-
+                    }
+                    if (st.charAt(cols)=='S' || st.charAt(cols)=='a' ) {
+                        startingNodes.add(newNode);
                     }
                 }
                 map.add(st);
                 rows++;
             }
 
-            char up ;
-            char down;
-            char left;
-            char right;
+            int shortestPath = findShortestPath(map, allNodes, source, target);
+            System.out.println("Day 12 - Question 2: "  + shortestPath);
 
-            Node currentNode;
-            rows = 0;
-            for (String nodes: map) {
-                for (int cols=0;cols<nodes.length();cols++) {
-                    char currentHeight = nodes.charAt(cols);
-                    int currentPos = cols+((nodes.length())*rows);
-                    currentNode = allNodes.get(currentPos);
+            int bestPath = shortestPath;
+            for (Node n: startingNodes) {
 
-                    // kijk boven
-                    if (rows>0) {
-                        up = map.get(rows-1).charAt(cols);
-
-                        if ((currentHeight+1)-up >=0) {
-                            //System.out.println("UP " + currentHeight + " " + up);
-                            currentNode.addDestination(allNodes.get(cols+(nodes.length()*(rows-1))),1);
-                         }
-                    }
-                    // kijk rechts
-                    if (cols<nodes.length()-1) {
-                        right = nodes.charAt(cols+1);
-                        if ((currentHeight+1)-right >=0) {
-                            //System.out.println("right " + currentHeight + " " + right);
-                            currentNode.addDestination(allNodes.get(cols+1+(nodes.length()*(rows))),1);
-                        }
-
-                    }
-                    // kijk onder
-                    if (rows<map.size()-1) {
-                        down = map.get(rows+1).charAt(cols);
-                        if ((currentHeight+1)-down >=0) {
-                            //System.out.println("down " + currentHeight + " " + down);
-                            currentNode.addDestination(allNodes.get(cols+(nodes.length()*(rows+1))),1);
-                        }
-                    }
-                    // kijk links
-                    if (cols>0) {
-                        left = nodes.charAt(cols-1);
-                        if ((currentHeight+1)-left >=0) {
-                            //System.out.println("left " + currentHeight + " " + left);
-                            currentNode.addDestination(allNodes.get(cols-1+(nodes.length()*(rows))),1);
-                        }
-                    }
-                    allNodes.set(currentPos,currentNode);
-
+                shortestPath = findShortestPath(map, allNodes, n, target);
+                if (shortestPath!=0 && shortestPath<bestPath) {
+                    bestPath = shortestPath;
                 }
-                rows++;
             }
-            Graph graph = new Graph();
-            for (Node n : allNodes) {
-                graph.addNode(n);
-            }
-            calculateShortestPathFromSource(graph,source);
-            Node n = graph.getNode(target);
-            System.out.println(n.getName() + n.getShortestPath());
-            System.out.println("Day 12 - Question 2: "  + n.getShortestPath().size());
-            System.out.println("Day 12 - Question 2: " );
+
+
+            System.out.println("Day 12 - Question 2: " + bestPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+    public static int findShortestPath(ArrayList<String> map, ArrayList<Node> allNodes, Node source, Node target) {
+        Node currentNode;
+        char up ;
+        char down;
+        char left;
+        char right;
+        int rows = 0;
+
+        for (String nodes : map) {
+            for (int cols = 0; cols < nodes.length(); cols++) {
+                char currentHeight = nodes.charAt(cols);
+                int currentPos = cols + ((nodes.length()) * rows);
+                currentNode = allNodes.get(currentPos);
+
+                // kijk boven
+                if (rows > 0) {
+                    up = map.get(rows - 1).charAt(cols);
+
+                    if ((currentHeight + 1) - up >= 0) {
+                        //System.out.println("UP " + currentHeight + " " + up);
+                        currentNode.addDestination(allNodes.get(cols + (nodes.length() * (rows - 1))), 1);
+                    }
+                }
+                // kijk rechts
+                if (cols < nodes.length() - 1) {
+                    right = nodes.charAt(cols + 1);
+                    if ((currentHeight + 1) - right >= 0) {
+                        //System.out.println("right " + currentHeight + " " + right);
+                        currentNode.addDestination(allNodes.get(cols + 1 + (nodes.length() * (rows))), 1);
+                    }
+
+                }
+                // kijk onder
+                if (rows < map.size() - 1) {
+                    down = map.get(rows + 1).charAt(cols);
+                    if ((currentHeight + 1) - down >= 0) {
+                        //System.out.println("down " + currentHeight + " " + down);
+                        currentNode.addDestination(allNodes.get(cols + (nodes.length() * (rows + 1))), 1);
+                    }
+                }
+                // kijk links
+                if (cols > 0) {
+                    left = nodes.charAt(cols - 1);
+                    if ((currentHeight + 1) - left >= 0) {
+                        //System.out.println("left " + currentHeight + " " + left);
+                        currentNode.addDestination(allNodes.get(cols - 1 + (nodes.length() * (rows))), 1);
+                    }
+                }
+                allNodes.set(currentPos, currentNode);
+
+            }
+            rows++;
+        }
+        Graph graph = new Graph();
+        for (Node n : allNodes) {
+            graph.addNode(n);
+        }
+        calculateShortestPathFromSource(source);
+        Node n = graph.getNode(target);
+        int size = n.getShortestPath().size();
+
+        for (Node n1 : allNodes) {
+            n1.setShortestPath(new LinkedList<>());
+        }
+
+        return size;
+    }
+
+
+    public static void calculateShortestPathFromSource(Node source) {
         source.setDistance(0);
 
         Set<Node> settledNodes = new HashSet<>();
@@ -125,7 +151,6 @@ public class Day12 {
             }
             settledNodes.add(currentNode);
         }
-        return graph;
     }
 
     private static Node getLowestDistanceNode(Set < Node > unsettledNodes) {
@@ -173,11 +198,6 @@ class Node {
 
     Map<Node, Integer> adjacentNodes = new HashMap<>();
 
-    public void printShortestPath() {
-        for (Node n : shortestPath) {
-            n.print();
-        }
-    }
 
     public void addDestination(Node destination, int distance) {
         adjacentNodes.put(destination, distance);
@@ -210,11 +230,5 @@ class Node {
         return this.distance;
     }
 
-    public void print() {
-        System.out.print(getName()+": ");
-        System.out.print(shortestPath.size());
-
-        System.out.println();
-    }
 
 }
